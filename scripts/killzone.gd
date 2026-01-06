3f64f3156c87568a9a198e962ae1f99fa117acee
shreejan <shr.shreejan@gmail.com>
2026-01-06 20:20:06 +1030

    Added enemy, remapped movement keys, and updated level layout

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,8 +3,11 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-    print("You died!")
-    timer.start()
+	print("You died!")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
+	timer.start()
 
 func _on_timer_timeout() -> void:
-    get_tree().reload_current_scene()
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
